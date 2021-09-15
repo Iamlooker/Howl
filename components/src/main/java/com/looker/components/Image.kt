487bba94014b683e5bec87d51cbc89a4914beb01
Mohit <mohit2002ss@gmail.com>
2021-09-15 13:16:30 +0530

    Image will crossfade on song change [Components]

--- components/src/main/java/com/looker/components/Image.kt
+++ components/src/main/java/com/looker/components/Image.kt
@@ -2,9 +2,9 @@ package com.looker.components
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.CornerBasedShape
@@ -18,11 +18,11 @@ import androidx.palette.graphics.Palette
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.looker.components.ComponentConstants.artworkUri
+import com.looker.components.ComponentConstants.tweenAnimation
 
 fun Long.bitmap(context: Context): Bitmap {
-    val uri: Uri? = this.artworkUri
-    return when (val source = uri?.let { ImageDecoder.createSource(context.contentResolver, it) }) {
-        null -> BitmapFactory.decodeResource(context.resources, R.drawable.error_image)
+    val uri: Uri = this.artworkUri
+    return when (val source = uri.let { ImageDecoder.createSource(context.contentResolver, it) }) {
         else -> ImageDecoder.decodeBitmap(source)
     }
 }
@@ -35,20 +35,25 @@ fun HowlImage(
     imageFillerColor: Color = MaterialTheme.colors.surface,
     shape: CornerBasedShape = MaterialTheme.shapes.medium
 ) {
-    Image(
-        modifier = modifier
-            .clip(shape)
-            .background(imageFillerColor),
-        contentScale = ContentScale.FillWidth,
-        painter = rememberImagePainter(
-            data = data,
-            builder = {
-                placeholder(R.drawable.white_background)
-                error(R.drawable.error_image)
-            }
-        ),
-        contentDescription = null
-    )
+    Crossfade(
+        targetState = data,
+        animationSpec = tweenAnimation()
+    ) {
+        Image(
+            modifier = modifier
+                .clip(shape)
+                .background(imageFillerColor),
+            contentScale = ContentScale.FillWidth,
+            painter = rememberImagePainter(
+                data = it,
+                builder = {
+                    placeholder(R.drawable.white_background)
+                    error(R.drawable.error_image)
+                }
+            ),
+            contentDescription = null
+        )
+    }
 }
 
 fun Bitmap?.getVibrantColor(): Color? = this?.let {
