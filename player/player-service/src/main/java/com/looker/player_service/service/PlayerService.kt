179c9174011c99355770ae4434ea3af0b26fb854
Mohit <mohit2002ss@gmail.com>
2021-09-20 02:26:38 +0530

    Use androidx [PlayerService]

--- player/player-service/src/main/java/com/looker/player_service/service/PlayerService.kt
+++ player/player-service/src/main/java/com/looker/player_service/service/PlayerService.kt
@@ -1,10 +1,12 @@
 package com.looker.player_service.service
 
-import android.app.Notification
 import android.app.Service
 import android.content.Intent
-import android.media.session.MediaSession
 import android.os.IBinder
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.core.app.NotificationCompat
+import androidx.media.session.MediaButtonReceiver
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.looker.constants.Constants.NOTIFICATION_CHANNEL_ID
 import com.looker.constants.Constants.NOTIFICATION_ID
@@ -18,21 +20,29 @@ class PlayerService : Service() {
     @Inject
     lateinit var player: SimpleExoPlayer
 
-    @Inject
-    lateinit var mediaSession: MediaSession
+    lateinit var mediaSession: MediaSessionCompat
 
     override fun onCreate() {
         super.onCreate()
 
-        val mediaStyle: Notification.MediaStyle =
-            Notification.MediaStyle().setMediaSession(mediaSession.sessionToken)
+        mediaSession = MediaSessionCompat(this, "howlmusic")
 
-        val notification = Notification.Builder(this, NOTIFICATION_CHANNEL_ID)
-            .setStyle(mediaStyle)
-            .setSmallIcon(R.drawable.ic_play)
-            .build()
+        val notification =
+            NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setStyle(
+                    androidx.media.app.NotificationCompat.MediaStyle()
+                        .setMediaSession(mediaSession.sessionToken)
+                        .setShowCancelButton(true)
+                        .setCancelButtonIntent(
+                            MediaButtonReceiver.buildMediaButtonPendingIntent(
+                                this,
+                                PlaybackStateCompat.ACTION_STOP
+                            )
+                        )
+                )
+                .setSmallIcon(R.drawable.ic_play)
 
-        startForeground(NOTIFICATION_ID, notification)
+        startForeground(NOTIFICATION_ID, notification.build())
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_STICKY
