fc2f4782aed7c0413237efd3f94ef11c51cb42c9
LooKeR <mohit2002ss@gmail.com>
2021-11-11 15:07:22 +0530

    Removed Unused Code [App]

--- app/src/main/java/com/looker/howlmusic/MainActivity.kt
+++ app/src/main/java/com/looker/howlmusic/MainActivity.kt
@@ -3,18 +3,13 @@ package com.looker.howlmusic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    val viewModel by viewModels<HowlViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent { App() }
         WindowCompat.setDecorFitsSystemWindows(window, false)
     }
