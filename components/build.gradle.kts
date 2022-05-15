677ccad19b454a762a49a8d4f5b0b8b61847a166
Iamlooker <mohit2002ss@gmail.com>
2022-05-15 13:38:56 +0530

    Reduce libraries

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -3,6 +3,7 @@ apply("$rootDir/android-library.gradle")
 val implementation by configurations
 
 dependencies {
+	implementation(Core.core)
 	implementation(Palette.palette)
 	implementation(Coil.coil)
 }
--- on-boarding/build.gradle.kts
+++ on-boarding/build.gradle.kts
@@ -3,6 +3,7 @@ apply("$rootDir/android-library.gradle")
 val implementation by configurations
 
 dependencies {
+	implementation(Compose.activity)
 	implementation(Compose.runtime)
 	implementation(Lifecycle.lifecycleViewModelCompose)
 }
