000245f328fa13d7d224fbfb5e1bfe13829bcb54
Iamlooker <mohit2002ss@gmail.com>
2022-06-16 01:58:49 +0530

    Release 0.2.4

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -4,6 +4,6 @@ object Android {
 	const val minSdk = 28
 	const val minDataSdk = 28
 	const val targetSdk = compileSdk
-	const val versionCode = 6
-	const val versionName = "0.2.3"
+	const val versionCode = 7
+	const val versionName = "0.2.4"
 }
