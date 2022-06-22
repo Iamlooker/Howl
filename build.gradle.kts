089d0fe2da1f417fb7fd2c98f55407799504b63a
Iamlooker <mohit2002ss@gmail.com>
2022-06-22 20:00:32 +0530

    New Gradle plugin method

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 buildscript {
-	repositories {
-		google()
-		mavenCentral()
-	}
 	dependencies {
-        classpath("com.android.tools.build:gradle:7.2.1")
-		classpath(Classpath.gradleKotlin)
-        classpath(Hilt.classpath)
+		classpath(Hilt.classpath)
 	}
 }
 
+plugins {
+	id("com.android.application") version "7.2.1" apply false
+	id("com.android.library") version "7.2.1" apply false
+	id("org.jetbrains.kotlin.android") version Version.kotlinVersion apply false
+}
+
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+	delete(rootProject.buildDir)
 }
