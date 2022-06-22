1ba5bf981abf488d4380583d331f85ee1c945b56
Iamlooker <mohit2002ss@gmail.com>
2022-06-22 13:03:25 +0530

    Fix build fails

--- feature-player/src/main/java/com/looker/feature_player/queue/QueueItem.kt
+++ feature-player/src/main/java/com/looker/feature_player/queue/QueueItem.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.looker.core_ui.TitleSubText
