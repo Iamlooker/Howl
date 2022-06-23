4f906707a3b8ca9cc3bb63c8a6a900fdeeb8935e
Iamlooker <mohit2002ss@gmail.com>
2022-06-23 15:48:29 +0530

    Upstream targetSdk to 33

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,6 +1,6 @@
 object Android {
 	const val appId = "com.looker.howlmusic"
-	const val compileSdk = 32
+	const val compileSdk = 33
 	const val minSdk = 21
 	const val versionCode = 8
 	const val versionName = "0.2.5"
