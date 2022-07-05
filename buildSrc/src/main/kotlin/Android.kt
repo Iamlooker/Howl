169bf1c4cc206293f0d09289e9e9790e73d0362f
Iamlooker <mohit2002ss@gmail.com>
2022-07-05 13:28:07 +0530

    Release Version 0.2.6

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,6 +2,6 @@ object Android {
 	const val appId = "com.looker.howlmusic"
 	const val compileSdk = 33
 	const val minSdk = 21
-	const val versionCode = 8
-	const val versionName = "0.2.5"
+	const val versionCode = 9
+	const val versionName = "0.2.6"
 }
